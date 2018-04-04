4ee5d4dfd11bdb3b5a26e90792ce33023ace0579
Mauricio Leon <iscmauleon@gmail.com>
2018-04-04 03:59:02 -0500

    Upload Local Copy

---
+++ ConnectFour - Unity Project/Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public GameObject redChip;
+	public GameObject blackChip;
+
+	public int[][] board = new int[7][6];
+
+
+	// Use this for initialization
+	void Start()
+	{
+		for(int i = 0; i < board.Length; i++)
+		{
+			
+		}
+		
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		
+	}
+}
---
+++ ConnectFour - Unity Project/Assets/Scripts/SelectionHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectionHandler : MonoBehaviour
+{
+	public 	GameObject chip;
+
+	void OnMouseEnter() {
+		
+	}
+
+	void OnMouseExit() {
+		Debug.Log("Sali");
+	}
+}
